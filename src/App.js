2e02bf826e933c11f9014cc0c4b22574363d7fbe
Mason Aviles <mce.aviles@gmail.com>
2023-05-23 20:16:31 -0700

    start over

--- src/App.js
+++ src/App.js
@@ -1,86 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { API_KEY } from "./config";
+import React, { useEffect } from "react";
 
 const App = () => {
-  const [imageFile, setImageFile] = useState(null);
-  const [results, setResults] = useState(null);
-  const [upscaleFactor, setUpscaleFactor] = useState(1);
-  const [noiseRemovalPercentage, setNoiseRemovalPercentage] = useState(0);
-  const [colorEnhancementPercentage, setColorEnhancementPercentage] = useState(0);
-  const [sharpeningPercentage, setSharpeningPercentage] = useState(0);
-
   useEffect(() => {
-    // Make the request to the API
-    if (imageFile) {
-      const formData = new FormData();
-      formData.append("image", imageFile);
-      formData.append("upscale_factor", upscaleFactor);
-      formData.append("noise_removal_percentage", noiseRemovalPercentage);
-      formData.append("color_enhancement_percentage", colorEnhancementPercentage);
-      formData.append("sharpening_percentage", sharpeningPercentage);
+    const url = 'https://ai-picture-upscaler.p.rapidapi.com/supersize-image';
+    const data = new FormData();
+    data.append('image', '');
+    data.append('sizeFactor', '2');
+    data.append('imageStyle', 'default');
+    data.append('noiseCancellationFactor', '0');
 
-      fetch("https://rapidapi.com/firdavscoder1/api/ai-picture-upscaler", {
-        method: "POST",
-        headers: {
-          "X-RapidAPI-Key": API_KEY
-        },
-        body: formData
-      })
-        .then((response) => response.json())
-        .then((results) => setResults(results));
-    }
-  }, [imageFile]);
+    const options = {
+      method: 'POST',
+      headers: {
+        'X-RapidAPI-Key': '810b826f54mshb2e4e8eab18545fp13dd8ajsn9dcd2fcf7fbe',
+        'X-RapidAPI-Host': 'ai-picture-upscaler.p.rapidapi.com'
+      },
+      body: data
+    };
 
-  return (
-    <div>
-      <input
-        type="file"
-        id="image-file"
-        onChange={(event) => setImageFile(event.target.files[0])}
-      />
-      <div>
-        <label>Upscale factor</label>
-        <input
-          type="range"
-          min="1"
-          max="4"
-          value={upscaleFactor}
-          onChange={(e) => setUpscaleFactor(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Noise removal percentage</label>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={noiseRemovalPercentage}
-          onChange={(e) => setNoiseRemovalPercentage(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Color enhancement percentage</label>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={colorEnhancementPercentage}
-          onChange={(e) => setColorEnhancementPercentage(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Sharpening percentage</label>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={sharpeningPercentage}
-          onChange={(e) => setSharpeningPercentage(e.target.value)}
-        />
-      </div>
-      {results && <img src={results.image} alt="Upscaled Image" />}
-    </div>
-  );
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Hello, React!</div>;
 };
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  API_KEY: "810b826f54mshb2e4e8eab18545fp13dd8ajsn9dcd2fcf7fbe",
+  API_KEY: "c904c576b9mshd3bd9ebbb036de5p1a5595jsn8e264820c294",
 };

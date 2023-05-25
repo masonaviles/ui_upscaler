289dca747a039a98311b7b350649241c136dda9e
Mason Aviles <mce.aviles@gmail.com>
2023-05-25 15:41:34 -0700

    update tool tip styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,8 @@ const Header = () => {
   return (
     <header className="p-5 py-4 mb-5 bg-blue-500">
       <h1 className="mb-4 text-2xl font-bold text-center text-white">🧐 Image AI Upscaler</h1>
-      <div className="p-2 bg-white rounded">
+      <div className={`p-2 bg-white rounded ${isDescriptionVisible ? 'w-full' : 'w-10'}`}>
         <div className="flex items-center">
-          <h2 className="flex-grow text-center">
-            <b>Use Image AI Upscaler</b>
-          </h2>
           <button
             className="flex items-center justify-center w-6 h-6 bg-gray-200 rounded-full"
             onClick={toggleDescription}
@@ -23,10 +20,13 @@ const Header = () => {
           </button>
         </div>
         {isDescriptionVisible && (
-          <div className="mb-4">
+          <div className="mx-1 my-2">
             <p>
               The AI-powered app is an image upscaling and enhancement tool. Using artificial intelligence algorithms, it can enhance the quality and resolution of images.
             </p>
+            <h2 className="flex-grow my-1">
+              <b>How to use Image AI Upscaler</b>
+            </h2>
             <ol className="pl-6 list-decimal">
               <li className="mb-2">Upload your image.</li>
               <li className="mb-2">Choose from a range of enhancement settings, such as sharpness adjustments or increasing the level of detail.</li>

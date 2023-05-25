cc479d0ea6f816b44793a1dbad99d7e928903825
Mason Aviles <mce.aviles@gmail.com>
2023-05-24 22:19:01 -0700

    Header.js: header copy update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="py-4 mb-5 bg-blue-500">
-      <h1 className="text-2xl font-bold text-center text-white">Image Upscaler</h1>
+      <h1 className="text-2xl font-bold text-center text-white">Image AI Upscaler</h1>
     </header>
   );
 };
